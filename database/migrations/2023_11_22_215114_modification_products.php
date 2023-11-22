bfc13a9aed48180f1223ec4b21a9e2d87eb6258b
DIGP1 <diegogarci098@gmail.com>
2023-11-22 15:53:49 -0600

    Modificacion de products

---
+++ database/migrations/2023_11_22_215114_modification_products.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->string('editorial');
+            $table->string('author');
+            $table->integer('year');
+            $table->string('category');
+            $table->longText('image');
+            $table->integer('stock');
+            $table->longText('description');
+            $table->float('price', 8,2)->default(0);
+            $table->float('sell_price', 8,2)->default(0);
+            $table->unsignedBigInteger('id_provider');
+            $table->foreign('id_provider')->references('id')->on('providers')->onDelete('cascade');
+            $table->integer('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
