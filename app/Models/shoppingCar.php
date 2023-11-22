ad20b249eb95baeb1256c85fc4c9f052152dae73
AngeLSanchez210 <is21001@ues.edu.sv>
2023-11-22 15:12:38 -0600

    metodos shopping

--- app/Models/shoppingCar.php
+++ app/Models/shoppingCar.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class shoppingCar extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'id_product',
+        'id_user',
+        'Cantidad',
+        'Total',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(products::class, 'id_product');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
