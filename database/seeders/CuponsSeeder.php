ded4f9dfcd8879f51d6614edccf1de0dfc3182b2
DIGP1 <diegogarci098@gmail.com>
2023-11-23 17:01:23 -0600

    Mas seeders

--- database/seeders/CuponsSeeder.php
+++ database/seeders/CuponsSeeder.php
@@ -4,46 +4,46 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class CuponsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        //
         $data = [
             [
                 'percent'=> 15.0,
                 'value'=> 0,
                 'create'=> '12/10/2023',
                 'expire'=> '30/11/2023',
-                'id_user'=> 1,
+                'id_user'=> 1
             ],
             [
                 'percent'=> 0,
                 'value'=> 2.00,
                 'create'=> '02/11/2023',
                 'expire'=> '02/12/2023',
-                'id_user'=> 1,
+                'id_user'=> 1
             ],
             [
                 'percent'=> 25.0,
                 'value'=> 0,
                 'create'=> '22/11/2023',
                 'expire'=> '30/11/2023',
-                'id_user'=> 2,
+                'id_user'=> 2
             ],
             [
                 'percent'=> 0,
                 'value'=> 14.00,
                 'create'=> '15/11/2023',
                 'expire'=> '28/11/2023',
-                'id_user'=> 2,
-            ],
+                'id_user'=> 2
+            ]
         ];
+        DB::table('cupons')->insert($data); 
     }
 }
